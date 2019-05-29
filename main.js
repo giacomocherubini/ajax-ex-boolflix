76767c226ba4057c02d220b7a968ae3dc2da56d4
giacomocherubini <giacomo.cherubini1@gmail.com>
2019-05-29 16:53:28 +0200

    Milestone 1 aggiunto funzioni

--- main.js
+++ main.js
@@ -3,24 +3,48 @@ $(document).ready(function() {
   var template_html = $("#template_film").html();
   var template_function = Handlebars.compile(template_html);
 
+  // intercetto il tasto invio
+  $('#search').keyup(function(event) {
+    if(event.which == 13) {
+      // l'utente ha premuto il tasto invio
+      // recupero il testo inserito dall'utente
+     var val_search = $('#search').val();
+     // faccio la chiamata ajax a tmdb
+     chiamata_api(val_search);
+    }
+  })
+
   $('#btn_search').click(function() {
     // recupero il testo inserito dall'utente
    var val_search = $('#search').val();
-   console.log(val_search);
-
    // faccio la chiamata ajax a tmdb
-  $.ajax({
-    'url': 'https://api.themoviedb.org/3/search/movie',
-    'data': {
-      'api_key': '98b8b4576cbd2fa355ca9f0c79a15767',
-      'query': val_search ,
-      'language': 'it'
-    },
-    'method': 'GET',
-    'success': function(data_response) {
-      // creo una variabile per l'array di tutti i film trovati
-      var movies = data_response.results;
-      // creo un ciclo for sull'array dei film
+   chiamata_api(val_search);
+  })
+
+   function chiamata_api (testo) {
+     // faccio la chiamata ajax a tmdb
+    $.ajax({
+      'url': 'https://api.themoviedb.org/3/search/movie',
+      'data': {
+        'api_key': '98b8b4576cbd2fa355ca9f0c79a15767',
+        'query': testo ,
+        'language': 'it'
+      },
+      'method': 'GET',
+      'success': function(data_response) {
+        // creo una variabile per l'array di tutti i film trovati
+        var movies = data_response.results;
+        stampa_film(movies);
+      },
+      'error': function() {
+        alert('si è verificato un errore');
+      }
+    })
+  }
+
+
+    function stampa_film(movies) {
+    // creo un ciclo for sull'array dei film
       for (var i = 0; i < movies.length; i++) {
         var movie = movies[i];
         var titolo = movie.title;
@@ -28,7 +52,7 @@ $(document).ready(function() {
         var linguaggio = movie.original_language;
         var voto = movie.vote_average;
         console.log(movie);
-        // creo l'ggetto Handlebars
+        // creo l'oggetto Handlebars
         var handlebars_film = {
           'title': titolo,
           'original_title': titolo_originale,
@@ -40,12 +64,8 @@ $(document).ready(function() {
         $('#films').append(html_film);
 
       }
-    },
-    'error': function() {
-      alert('si è verificato un errore');
     }
-  })
 
- })
+
 
 });
